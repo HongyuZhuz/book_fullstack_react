2a205c6a584dd2070b9355c1b08145e8d9062f83
HongyuZhuz <Hongyu.Zhu.zz@gmail.com>
2023-12-01 11:34:14 +1100

    built part of static version of the app

--- time_tracking_app/public/js/app.js
+++ time_tracking_app/public/js/app.js
@@ -1,1 +1,8 @@
-
+class TimersDashboard extends React.Component{
+    render(){
+        return{
+            <div className = 'ui three column centered grid'>
+                <
+        }
+    }
+}
