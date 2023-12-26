f320fa2ef6e260846a72dabcb5ea65e6bf342306
HongyuZhuz <Hongyu.Zhu.zz@gmail.com>
2023-12-26 17:48:54 +1100

    try to build connection with server

--- time_tracking_app/public/js/app.js
+++ time_tracking_app/public/js/app.js
@@ -1,21 +1,14 @@
 class TimersDashboard extends React.Component{
     state = {
         timers:[
-            {
-                title:'Practice squat',
-                project: 'Gym Chores',
-                id:uuid.v4(),
-                elapsed:5456099,
-                runningSince:Date.now(),
-            },
-            {
-                title: 'Bake squash',
-                project:'Kichen Chores',
-                id:uuid.v4(),
-                elapsed:1273998,
-                runningSince:null,
-            },
         ]
+    }
+    componentDidMount(){
+        this.loadTimersFromServer();
+        setInterval(this.loadTimersFromServer,5000);
+    }
+    loadTimersFromServer =()=>{
+        client.getTimers((serverTimers)=>(this.setState({timers:serverTimers})))
     }
 
     handleCreateFormSubmit = (timer)=>{
